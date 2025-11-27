6722146bc4c48b0d277011ed541bd0be464f6a9c
Timothy Maurer <timothygmaurer@outlook.com>
2025-11-27 20:31:12 +0100

    Add Inter font

---
+++ tailwind.config.ts
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx,html}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Inter", "system-ui", "Avenir", "Helvetica", "Arial", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
