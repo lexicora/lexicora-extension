513966c906c82b0f0033c3c909951365d9d3ae8b
Timothy Maurer <timothygmaurer@outlook.com>
2025-11-27 19:46:36 +0100

    Comment out potentially unnecessary code

--- src/entrypoints/popup/App.tsx
+++ src/entrypoints/popup/App.tsx
@@ -18,7 +18,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1 className="test">WXT + React</h1>
+      <h1 className="test-app">WXT + React</h1>
       <div className="card">
         <div style={{ display: 'flex', gap: '10px', justifyContent: 'center', marginBottom: '10px' }}>
             <Button onClick={() => setCount((count) => count + 1)}>
