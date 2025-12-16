fe4ca82404a11881fdb71e0c598593c56d9d1e32
Timothy Maurer <timothygmaurer@outlook.com>
2025-12-16 15:57:40 +0100

    Remove Inter and Figtree font

--- src/entrypoints/sidepanel/main.tsx
+++ src/entrypoints/sidepanel/main.tsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
-//import "@fontsource/inter/index.css";
-//import '@fontsource/figtree/index.css';
 import '@fontsource/wix-madefor-text/400.css'
 import '@fontsource/wix-madefor-text/400-italic.css'
 import '@fontsource/wix-madefor-text/500.css'
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,8 +6,6 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        // sans: ["Inter", "system-ui", "Avenir", "Helvetica", "Arial", "sans-serif"],
-        //sans: ["Figtree", "system-ui", "Avenir", "Helvetica", "Arial", "sans-serif"],
         sans: ["Wix Madefor Text", "system-ui", "Avenir", "Helvetica", "Arial", "sans-serif"],
       },
     },
