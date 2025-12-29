cd9f28d05dc482d81ad4a1479b02099fa69514ad
Timothy Maurer <timothygmaurer@outlook.com>
2025-12-29 20:13:08 +0100

    Update style of app BlockNote editor

--- src/entrypoints/sidepanel/main.tsx
+++ src/entrypoints/sidepanel/main.tsx
@@ -11,7 +11,9 @@ import "@fontsource/wix-madefor-text/700.css";
 import "@fontsource/wix-madefor-text/700-italic.css";
 //import '@fontsource/wix-madefor-text/800.css'
 //import '@fontsource/wix-madefor-text/800-italic.css'
-import "@fontsource/jetbrains-mono";
+//import "@fontsource/jetbrains-mono/200.css";
+import "@fontsource/jetbrains-mono/300.css";
+import "@fontsource/jetbrains-mono/400.css";
 import "../../assets/styles/global.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
