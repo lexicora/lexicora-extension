912333cc9f6eccf7ee6137ab348bc16a810d56ab
Timothy Maurer <timothygmaurer@outlook.com>
2026-01-19 19:45:54 +0100

    feat(styles): adjust UI for better accessibility

--- src/entrypoints/sidepanel/pages/entries/new/index.tsx
+++ src/entrypoints/sidepanel/pages/entries/new/index.tsx
@@ -129,9 +129,10 @@ function NewEntryPage() {
               //rows={4}
               maxLength={500}
               placeholder="Type your desired AI prompt here."
-              className={`text-base! field-sizing-content resize-none max-h-88.5 min-h-10.5 focus-visible:ring-0 backdrop-blur-lg dark:bg-[#171717dd] bg-[#fdfdfddd] scrollbar-thin ${
-                import.meta.env.FIREFOX ? "resize-y h-10.5" : ""
-              }`} // NOTE (feature parity discrepancy): No support fo field sizing content in Firefox and also different behavior compared to Chrome
+              className={`shadow-[0_-6px_6px_0px_var(--color-gray-300)]/25 dark:shadow-[0_-6px_6px_0px_var(--background)]/25
+                text-base! field-sizing-content resize-none max-h-88.5 min-h-10.5 focus-visible:ring-0 backdrop-blur-lg dark:bg-[#171717dd] bg-[#fdfdfddd] scrollbar-thin ${
+                  import.meta.env.FIREFOX ? "resize-y h-10.5" : ""
+                }`} // NOTE (feature parity discrepancy): No support fo field sizing content in Firefox and also different behavior compared to Chrome
               value={promptText} // 3. Bind the state to the value prop
               onChange={(e) => setPromptText(e.target.value)} // 4. Update state on every keystroke
               onKeyDown={(e) => {
