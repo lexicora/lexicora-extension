5827e37297d56ca852b1c507b2d12dbc0bb9e714
Timothy Maurer <timothygmaurer@outlook.com>
2026-01-21 16:25:58 +0100

    feat(sidepanel): add base structure of settings page

--- src/entrypoints/sidepanel/pages/settings/index.tsx
+++ src/entrypoints/sidepanel/pages/settings/index.tsx
@@ -1,5 +1,19 @@
 function SettingsPage() {
-  return <div>Settings Page</div>;
+  return (
+    <div className="lc-page-container">
+      <header className="mb-4">
+        <h1 className="text-xl font-bold">Settings</h1>
+      </header>
+      <main>
+        <section id="personalization-section">
+          <article id=""></article>
+        </section>
+        <section id="notification-section">
+          <article></article>
+        </section>
+      </main>
+    </div>
+  );
 }
 
 export default SettingsPage;
