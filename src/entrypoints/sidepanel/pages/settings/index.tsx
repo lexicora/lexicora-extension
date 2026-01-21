5de7bbcfa64803c5a3f104349223fa208ae42b05
Timothy Maurer <timothygmaurer@outlook.com>
2026-01-21 21:23:13 +0100

    feat(sidepanel): disable selection of elements in settings page

--- src/entrypoints/sidepanel/pages/settings/index.tsx
+++ src/entrypoints/sidepanel/pages/settings/index.tsx
@@ -1,6 +1,6 @@
 function SettingsPage() {
   return (
-    <div className="lc-page-container">
+    <div className="lc-page-container select-none">
       <header className="mb-4">
         <h1 className="text-xl font-bold">Settings</h1>
       </header>
