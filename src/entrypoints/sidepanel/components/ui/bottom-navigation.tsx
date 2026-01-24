eb46b3cf799abbac6476f4e6f7fcd74765321ee0
Timothy Maurer <timothygmaurer@outlook.com>
2026-01-24 21:30:50 +0100

    feat(sidepanel): improve bottom-navigation

--- src/entrypoints/sidepanel/components/ui/bottom-navigation.tsx
+++ src/entrypoints/sidepanel/components/ui/bottom-navigation.tsx
@@ -1,22 +1,17 @@
 import "./bottom-navigation.css";
 import {
-  HomeIcon, //Home page
-  RectangleStackIcon, //Candidate for Entries page 1
-  Bars2Icon, //Candidate for Entries page 2
-  Bars3Icon, //Candidate for Entries page 3
-  Bars4Icon, //Candidate for Entries page 4
+  //HomeIcon, //Home page
+  RectangleStackIcon, // TODO: maybe switch to this later (for Entries page)
   Cog6ToothIcon, //Settings page
 } from "@heroicons/react/24/outline";
-import { HomeIconOutline as CustomHeroHomeIcon } from "@/components/icons/custom-heroicons";
+import { HomeIconOutline as CustomHeroHomeIcon } from "@/components/icons/custom-heroicons"; //(Custom icon) Home page
 import {
-  HomeIcon as HomeIconSolid, //Home page
-  RectangleStackIcon as RectangleStackIconSolid, //Candidate for Entries page 1
+  HomeIcon as HomeIconSolid, //Home page (solid)
+  RectangleStackIcon as RectangleStackIconSolid, // TODO: maybe switch to this later (for Entries page)
   Cog6ToothIcon as Cog6ToothIconSolid, //Settings page
 } from "@heroicons/react/24/solid";
 import { StretchHorizontalIcon } from "lucide-react"; //Candidate for Entries page 5 (solid)
 import { NavLink, useLocation, matchPath } from "react-router-dom";
-
-//<HugeiconsIcon icon={ListViewIcon} /> //Notable mention as Candidate for Entries page (not 100% free)
 
 export function BottomNavigation() {
   const { pathname } = useLocation();
@@ -42,21 +37,21 @@ export function BottomNavigation() {
           `lc-bottom-navigation ${isHidden ? "lc-bottom-navigation--hidden" : ""}
           fixed bottom-0 w-full h-14.75 px-3 z-100 select-none
           border-t bg-background/80 backdrop-blur-lg
-          transition-shadow duration 300` /*TODO: Shadow effects */
+          transition-shadow duration 300` /*TODO: Shadow effects (logic necessary: hide when scrolled to bottom)*/
         }
       >
         <div
           className="flex gap-3 items-center justify-between text-center w-full max-w-317 h-full /*max-w-7xl*/ mx-auto inset-x-0"
           // had classes: lc-bottom-navigation-animate-blur ${isHidden ? "lc-bottom-navigation-animate-blur--hidden" : ""}
         >
-          <div className="flex-1 mx-1.5 ml-0.5">
+          <div className="flex-1 mx-1.5 ml-0.5 flex items-center justify-center h-full">
             {/* Home */}
             <NavLink
               to="/"
               title={pathname !== "/" ? "Home" : ""}
               draggable={false}
               viewTransition
-              className="group flex flex-col items-center py-3 w-full"
+              className="group flex flex-col items-center py-4 w-full"
             >
               {({ isActive }) => (
                 <div
@@ -71,14 +66,14 @@ export function BottomNavigation() {
               )}
             </NavLink>
           </div>
-          <div className="flex-1 mx-1.5">
+          <div className="flex-1 mx-1.5 flex items-center justify-center h-full">
             {/* Entries */}
             <NavLink
               to="/entries"
               title={pathname !== "/entries" ? "Entries" : ""}
               draggable={false}
               viewTransition
-              className="group flex flex-col items-center py-3 w-full"
+              className="group flex flex-col items-center py-4 w-full"
             >
               {({ isActive }) => (
                 <div
@@ -93,14 +88,14 @@ export function BottomNavigation() {
               )}
             </NavLink>
           </div>
-          <div className="flex-1 mx-1.5 mr-0.5">
+          <div className="flex-1 mx-1.5 mr-0.5 flex items-center justify-center h-full">
             {/* Settings */}
             <NavLink
               to="/settings"
               title={pathname !== "/settings" ? "Settings" : ""}
               draggable={false}
               viewTransition
-              className="group flex flex-col items-center py-3 w-full"
+              className="group flex flex-col items-center py-4 w-full"
             >
               {({ isActive }) => (
                 <div
