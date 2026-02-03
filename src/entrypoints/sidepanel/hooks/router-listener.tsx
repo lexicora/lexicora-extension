dd91576e602e711644902bd985474c2649ced93e
Timothy Maurer <timothygmaurer@outlook.com>
2026-02-03 18:51:56 +0100

    refactor: clean up code and update blocknote style selectors

--- src/entrypoints/sidepanel/hooks/router-listener.tsx
+++ src/entrypoints/sidepanel/hooks/router-listener.tsx
@@ -22,18 +22,6 @@ export function RouterListener() {
           viewTransition: true,
         });
         return true; //* NOTE: To signal to clear the location of pending navigation in the background or other scripts.
-        // const targetPath = msg.data.path;
-        // const isAlreadyOnPath = location.pathname === targetPath;
-
-        // if (targetPath && !isAlreadyOnPath) {
-        //   navigate(targetPath, { viewTransition: true });
-        // } else {
-        //   navigate(targetPath, {
-        //     state: { reload: true }, // pass state to component, then in component const location = useLocation(); if(location.state.reload) { do something }
-        //     viewTransition: true,
-        //   });
-        //   //window.location.reload();
-        // }
       },
     );
 
@@ -43,20 +31,19 @@ export function RouterListener() {
   }, [navigate, location]);
 
   useEffect(() => {
-    const navigateToLocation = async () =>
-      //destination: "background" | "popup" | "content",
-      {
-        const path = await sendMessage<string | null>(
-          MSG.REQUEST_PENDING_NAVIGATION,
-          {},
-          "background", //destination,
-        ).catch(() => {});
-        if (path) {
-          if (path === location.pathname) return;
-          navigate(path, { viewTransition: true });
-        }
-      };
+    const navigateToLocation = async () => {
+      const path = await sendMessage<string | null>(
+        MSG.REQUEST_PENDING_NAVIGATION,
+        {},
+        "background", //destination,
+      ).catch(() => {});
+      if (path) {
+        if (path === location.pathname) return;
+        navigate(path, { viewTransition: true });
+      }
+    };
     navigateToLocation();
+    // parameter: destination: "background" | "popup" | "content",
     // navigateToLocation("background");
     // navigateToLocation("content");
     // navigateToLocation("popup");
