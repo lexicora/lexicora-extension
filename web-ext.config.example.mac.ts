e75e164299d74d9083f60abc64f8c82346a4e301
Timothy Maurer <tgmaurer@icloud.com>
2026-09-13 01:57:46 +0200

    docs(web-ext): update example config files

--- web-ext.config.example.mac.ts
+++ web-ext.config.example.mac.ts
@@ -4,6 +4,6 @@ export default defineWebExtConfig({
   binaries: {
     //chrome: "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome", // Chrome is already known to web-ext.
     edge: "/Applications/Microsoft Edge.app/Contents/MacOS/Microsoft Edge",
-    firefox: "/Applications/Firefox.app/Contents/MacOS/firefox",
+    //firefox: "/Applications/Firefox.app/Contents/MacOS/firefox", // Firefox might already be known to web-ext.
   },
 });
--- web-ext.config.example.windows.ts
+++ web-ext.config.example.windows.ts
@@ -4,6 +4,6 @@ export default defineWebExtConfig({
   binaries: {
     //chrome: "C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe", // Chrome is already known to web-ext.
     edge: "C:\\Program Files (x86)\\Microsoft\\Edge\\Application\\msedge.exe",
-    firefox: "C:\\Program Files\\Mozilla Firefox\\firefox.exe",
+    //firefox: "C:\\Program Files\\Mozilla Firefox\\firefox.exe", // Firefox might already be known to web-ext.
   },
 });
