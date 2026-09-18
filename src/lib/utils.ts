c029ca15cb6f757d7aac24e28d0a3b96465783e8
Timothy Maurer <tgmaurer@icloud.com>
2026-09-18 17:48:09 +0200

    docs(utils): add code comment regarding migration of cn function

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,6 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+// TODO: Replace with the faster cn package: https://npmx.dev/package/cn / https://www.npmjs.com/package/cn
+// INFO: It is a drop in replacement for clsx and twMerge. See issue: #198
